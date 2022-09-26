07326c341e4696813ed04ebbc67962fe9865a311
Sandeep Kumar <sandeepmahendran@gmail.com>
2022-09-26 16:25:37 +0530

    D

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = Express();
 
 app.use(Express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path + "\n");
+});
+
 app.post("/webhook/search", async (req, res, next) => {
   console.log(req.body);
   res.status(200).json({
