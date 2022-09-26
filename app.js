f4f1b80e30a9257740047a3173bfe77f8608bd44
Sandeep Kumar <sandeepmahendran@gmail.com>
2022-09-26 16:27:31 +0530

    sd

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post("/webhook", async (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({
+    message: "OK",
+  });
+});
+
 app.post("/webhook/search", async (req, res, next) => {
   console.log(req.body);
   res.status(200).json({
